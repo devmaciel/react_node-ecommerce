a0798f6128012a3095dbf6211e5d008bfe8c07f6
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-25 16:35:19 -0300

    react hook

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+// import data from '../data';
+import axios from 'axios';
 
 function HomeScreen (props) {
+
+    //proxy react hooks
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        
+        const fetchData = async() => {
+            const {data} = await axios.get("/api/products");
+            setProduct(data)
+        }
+
+        fetchData();
+
+        return () => {
+
+        };
+    }, [])
+
+
     return <div>
         <ul className="products">
             {
-                data.products.map(product => 
-                    <li>
+                products.map(product => 
+                    <li key={product._id}>
                         <div className="product">
                             <Link to={'/product/' + product._id}>
                                 <img className="product-image" src={product.image} alt="product" />
